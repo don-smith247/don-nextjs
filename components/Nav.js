acbe57f9c939f6a7cf8416176897a2822a179f65
Abdullahi Kolawole <smithdon578@gmail.com>
2024-08-12 21:30:16 +0100

    first commit

--- components/Nav.js
+++ components/Nav.js
@@ -25,9 +25,26 @@ export const navData = [
     icon: <HiEnvelope />,
   },
 ];
+ import Link from 'next/link';
 
+ import {useRouter} from 'next/router'
 const Nav = () => {
-  return <nav>nav</nav>;
+  const router = useRouter();
+  const pathname = router.pathname;
+  
+  return (
+  <nav className=' flex flex-col  items-center xl:justify-center fixed h-max bottom-0 mt-auto xl:right-[2%] z-50 top-0 w-full xl:w-16 xl:max-w-md xl:h-screen  '>
+    <div className='flex w-full xl:flex-col items-center justify-between xl:justify-center gap-y-10 px-4 md:px-40 xl:px-0 h-[80px]  xl:max-h py-8 bg-white/10  backdrop-blur-sm text-3xl xl:text-xl xl:rounded-full  '>
+        {navData.map((link, index) => {
+          return (
+            <a className='' key={index} href={link.path}>
+              <div>{link.icon}</div>
+            </a>
+          );
+        })}
+      </div>
+  </nav>
+  );
 };
 
 export default Nav;
