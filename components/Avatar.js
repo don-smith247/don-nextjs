3acb0aebdbb6d9368bf2de1b3de36e53e2643aa5
Abdullahi Kolawole <smithdon578@gmail.com>
2024-08-18 18:33:10 +0100

    third commit

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,5 +1,12 @@
+import Image from "next/image";
+
+
 const Avatar = () => {
-  return <div>Avatar</div>;
+  return (
+    <div className=" hidden xl:flex xl:max-w-none ">
+    <Image src={'/animer.png'} width={500} height={400} alt="" className=" translate-z-0 absolute -bottom-32 lg:bottom-0 lg:right-[8%]"/>
+  </div>
+  );
 };
 
 export default Avatar;
