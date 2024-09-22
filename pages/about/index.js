5ab2e75865baa5ec231fef08ea2c56007026b81c
Abdullahi Kolawole <smithdon578@gmail.com>
2024-09-22 15:01:39 +0100

    error fixed

--- pages/about/index.js
+++ pages/about/index.js
@@ -122,8 +122,8 @@ const About = () => {
           initial="hidden"
           animate="show"
           exit="hidden">
-            5 years ago, i began freelancing as a developer. since then, i've done remote work for agencies, counsulted
-             for startups, and collaborated on digital product for and consumer use.  
+            5 years ago, I began freelancing as a developer. Since then, I&apos;ve done remote work for agencies, consulted
+             for startups, and collaborated on digital products for consumer use.  
           </motion.p>
           <motion.div className=" hidden md:flex md:max-w-xl xl:max-w-none mx-auto xl:mx-0 "
           variants={fadeIn("right", 0.6)}
@@ -168,33 +168,28 @@ const About = () => {
          animate="show"
          exit="hidden">
            <div className="flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4 ">
-             {aboutData.map ((item, itemIndex)=> {
-              return(
+             {aboutData.map((item, itemIndex) => (
               <div key={itemIndex} 
-                className={`${index === itemIndex && 'text-accent after:w-[100%] after:bg-accent after:transition-all after:duration-300'} cursor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px]
-                after:bg-white after:absolute after:-bottom-1 after:left-0`}
-                onClick={()=> setIndex(itemIndex)}
+                className={`${index === itemIndex && 'text-accent after:w-[100%] after:bg-accent after:transition-all after:duration-300'} cursor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px] after:bg-white after:absolute after:-bottom-1 after:left-0`}
+                onClick={() => setIndex(itemIndex)}
                 >
                   {item.title}
               </div>
-              );
-             })}
+             ))}
            </div>
-           <div className=" py-2 xl:py-6 flex flex-col gap-y-2 xl:gap-y-4 items-center xl:items-start">
-            {aboutData[index].info.map((item, itemIndex)=>{
-              return(
-                <div key={itemIndex} className=" flex-1 flex flex-col md:flex-row max-w-max gap-x-2 items-center text-white/60 ">
-                   <div className=" font-light md-2 md:mb-0 ">{item.title}</div>
-                   <div className=" hidden md:flex">-</div>
-                   <div>{item.stage}</div>
-                   <div className=" flex gap-x-4 ">
-                     {item.icons?.map((icon, itemIndex)=>{
-                      return <div className=" text-3xl text-white">{icon}</div>
-                     })}
-                   </div>
+           <div className="py-2 xl:py-6 flex flex-col gap-y-2 items-center xl:items-start ">
+            {aboutData[index].info.map((item, itemIndex) => (
+              <div key={itemIndex} className="flex-1 flex flex-col md:flex-row max-w-max gap-x-2 items-center text-white/60">
+                <div className="font-light md-2 md:mb-0">{item.title}</div>
+                <div className="hidden md:flex">-</div>
+                <div>{item.stage}</div>
+                <div className="flex gap-x-4">
+                  {item.icons?.map((icon, iconIndex) => (
+                    <div key={iconIndex} className="text-3xl text-white">{icon}</div>
+                  ))}
                 </div>
-              );
-            })}
+              </div>
+            ))}
            </div>
          </motion.div>
       </div>
