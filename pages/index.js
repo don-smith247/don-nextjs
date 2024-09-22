062464f226f24e45bf40fa39aeffd3dfa6b9f3e7
Abdullahi Kolawole <smithdon578@gmail.com>
2024-09-22 14:44:24 +0100

    all set

--- pages/index.js
+++ pages/index.js
@@ -28,8 +28,8 @@ const Home = () => {
     <div className="w-[1200px] h-full absolute right-0 bottom-0 ">
       <div className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0"></div>
       <ParticlesContainer/>
-      <motion.div>
-        <Avatar/>
+      <motion.div variants={fadeIn('up', 0.4 )} initial="hidden" animate="show" exit="hidden">
+        <Avatar />
       </motion.div>
     </div>
   </div>
--- pages/testimonials/index.js
+++ pages/testimonials/index.js
@@ -1,5 +1,28 @@
+import TestimonialSlider from "../../components/TestimonialSlider";
+import {motion} from 'framer-motion'
+import {fadeIn} from '../../variants';
 const Testimonials = () => {
-  return <div>Testimonials</div>;
+  return (
+    <div className=" h-full bg-primary/30 py-32">
+        <div className=" container mx-auto h-full flex flex-col justify-center  ">
+          <motion.h2 className="h2 text-white mb-8 xl:mb-0 text-center"
+           variants={fadeIn("up", 0.6)}
+           initial="hidden"
+           animate="show"
+           exit="hidden">
+            What client <span className=" text-accent">say.</span>
+          </motion.h2>
+          <motion.div
+          variants={fadeIn("right", 0.6)}
+          initial="hidden"
+          animate="show"
+          exit="hidden"
+          >
+            <TestimonialSlider/> 
+          </motion.div>
+        </div>
+    </div>
+  );
 };
 
 export default Testimonials;
